e08826eb68b3a0a36c5fc71886b10a032ad1104f
rorysaur <ou.rory@gmail.com>
2023-07-20 22:48:35 -0400

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen px-32 py-24 flex flex-col items-center">
-      <div className="text-xl my-2">for book lovers.</div>
+      <div className="text-xl my-2">for book people.</div>
       <div className="text-xl my-2">coming soon!</div>
     </main>
   )
