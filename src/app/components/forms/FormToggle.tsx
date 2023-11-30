9175cb322c2573da3c768ff34a91eac5a13e70c5
Brian McGue <mcgue.brian@gmail.com>
2023-11-30 18:22:31 -0500

    Shrink the toggle

--- src/app/components/forms/FormToggle.tsx
+++ src/app/components/forms/FormToggle.tsx
@@ -33,13 +33,13 @@ export default function FormToggle({
             checked={field.value}
             onChange={field.onChange}
             className={`${field.value ? "bg-gold-500" : "bg-gold-900"}
-            relative inline-flex h-[38px] w-[74px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white/75`}
+            relative inline-flex items-center h-[26px] w-[50px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white/75`}
           >
             <span className="sr-only">{label}</span>
             <span
               aria-hidden="true"
-              className={`${field.value ? "translate-x-9" : "translate-x-0"}
-              pointer-events-none inline-block h-[34px] w-[34px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+              className={`${field.value ? "translate-x-6" : "translate-x-0.5"}
+              pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
             />
           </Switch>
         )}
