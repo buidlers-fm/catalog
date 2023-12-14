0a0be0dd2809a7ed80c75388b790b1ccac0cc3f2
rorysaur <ou.rory@gmail.com>
2023-12-13 20:51:57 -0500

    redirect invalid lists/new route

--- src/app/users/[username]/lists/new/page.tsx
+++ src/app/users/[username]/lists/new/page.tsx
@@ -1,21 +1,28 @@
 import { redirect } from "next/navigation"
 import OpenLibrary from "lib/openLibrary"
 import { getCurrentUserProfile } from "lib/server/auth"
+import { getUserListsLink } from "lib/helpers/general"
 import EditList from "app/users/[username]/lists/new/components/EditList"
 import type Book from "types/Book"
 
 export const dynamic = "force-dynamic"
 
-export default async function CreateListPage({ searchParams }) {
-  const userProfile = await getCurrentUserProfile()
-  if (!userProfile) redirect("/")
+export default async function CreateListPage({ params, searchParams }) {
+  const currentUserProfile = await getCurrentUserProfile()
+
+  // redirect if not signed in
+  if (!currentUserProfile) redirect("/")
+
+  // redirect if not on own profile
+  const { username } = params
+  if (username !== currentUserProfile.username) redirect(getUserListsLink(username))
 
   const { with: openLibraryWorkId } = searchParams
 
   if (openLibraryWorkId) {
     const openLibraryBook: Book = await OpenLibrary.getFullBook(openLibraryWorkId)
-    return <EditList currentUserProfile={userProfile} firstBook={openLibraryBook} />
+    return <EditList currentUserProfile={currentUserProfile} firstBook={openLibraryBook} />
   } else {
-    return <EditList currentUserProfile={userProfile} />
+    return <EditList currentUserProfile={currentUserProfile} />
   }
 }
