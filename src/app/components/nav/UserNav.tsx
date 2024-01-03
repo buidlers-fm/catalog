6d674689d6a33664de94ce9b55ffabafb94f82c5
Brian McGue <mcgue.brian@gmail.com>
2024-01-03 16:39:51 -0500

    Make sure user nav dropdown appears in front of homepage image

--- src/app/components/nav/UserNav.tsx
+++ src/app/components/nav/UserNav.tsx
@@ -76,7 +76,7 @@ export default function UserNav({ currentUserProfile: _initialCurrentUserProfile
             </div>
           </Menu.Button>
           <div className="relative">
-            <Menu.Items className="absolute top-2 w-[108px] bg-gray-900 rounded">
+            <Menu.Items className="absolute top-2 w-[108px] bg-gray-900 rounded z-10">
               <Menu.Item>
                 <Link href="/home">
                   <button className="w-full cat-btn-text hover:bg-gray-700 px-4 pt-3 pb-2 text-left rounded-tl rounded-tr">
