20fba4c1c0fd9ef5a6dcb4d1a97db4c0101e5f43
rorysaur <ou.rory@gmail.com>
2024-02-15 23:48:09 -0500

    fix failing test

--- tests/prisma.test.ts
+++ tests/prisma.test.ts
@@ -1,32 +1,32 @@
 import { v4 as uuidv4 } from "uuid"
 import prisma from "lib/prisma"
 
+const USER_EMAIL = "test@test.com"
+
 describe("testing jest-prisma", () => {
-  let initialUserCount
-
-  beforeAll(async () => {
-    initialUserCount = await jestPrisma.originalClient.user.count()
-  })
-
-  test("Create a user", async () => {
+  test("create a user", async () => {
     const createdUser = await prisma.user.create({
       data: {
         id: uuidv4(),
-        email: "test@test.com",
+        email: USER_EMAIL,
       },
     })
 
     expect(
       await prisma.user.findFirst({
         where: {
-          email: "test@test.com",
+          email: USER_EMAIL,
         },
       }),
     ).toStrictEqual(createdUser)
   })
 
-  test("num users has not changed", async () => {
-    const currentUserCount = await prisma.user.count()
-    expect(currentUserCount).toBe(initialUserCount)
+  test("user created in other test does not exist", async () => {
+    const user = await prisma.user.findFirst({
+      where: {
+        email: USER_EMAIL,
+      },
+    })
+    expect(user).toBeNull()
   })
 })
