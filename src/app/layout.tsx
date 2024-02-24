5d5050cbb9d3bdca504ce7a9dc8da88873290bb0
Rory Ou <ou.rory@gmail.com>
2024-02-23 22:04:53 -0500

    bluesky logo + update changelog (#140)
    using svg, but saving the logo image in case we need it later.
    
    bluesky svg/assets source:
    FortAwesome/Font-Awesome#19810 (comment)
    https://drive.google.com/drive/folders/1RDpuQOQMfM9mXQ61wUYWNZUbgvDc8r-n

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { UserProvider } from "lib/contexts/UserContext"
 import { NotificationsProvider } from "lib/contexts/NotificationsContext"
 import CatalogHeader from "app/components/CatalogHeader"
 import Toast from "app/components/Toast"
+import BlueskyLogo from "app/components/BlueskyLogo"
 import type { Metadata } from "next"
 
 const chivoMono = Chivo_Mono({ subsets: ["latin"], variable: "--font-chivo-mono" })
@@ -44,28 +45,41 @@ export default async function RootLayout({ children }: { children: React.ReactNo
               <main className="mb-auto font-newsreader font-normal text-md tracking-wide leading-relaxed">
                 {children}
               </main>
-              <footer className="mt-32 px-6 xs:px-8 py-4 flex font-chivo-mono tracking-wider">
-                ©&nbsp;
-                <Link href="/">catalog</Link>.
-                <div className="ml-4 xs:ml-6">
-                  <Link href="/news" className="cat-btn-link text-sm">
-                    news
-                  </Link>
+              <footer className="mt-32 px-6 xs:px-8 py-4 flex flex-col xs:flex-row font-chivo-mono tracking-wider">
+                <div className="flex">
+                  ©&nbsp;
+                  <Link href="/">catalog</Link>.
+                  <div className="ml-4 xs:ml-6">
+                    <Link href="/news" className="cat-btn-link text-sm">
+                      news
+                    </Link>
+                  </div>
+                  <div className="ml-4 xs:ml-6">
+                    <Link href="/changelog" className="cat-btn-link text-sm">
+                      changelog
+                    </Link>
+                  </div>
                 </div>
-                <div className="ml-4 xs:ml-6">
-                  <Link href="/changelog" className="cat-btn-link text-sm">
-                    changelog
-                  </Link>
-                </div>
-                <div className="ml-4 xs:ml-6">
-                  <a href="mailto:staff@catalog.fyi">
-                    <BsEnvelopeFill className="mt-[5px] text-lg" />
-                  </a>
-                </div>
-                <div className="ml-4 xs:ml-6">
-                  <a href="https://discord.gg/BWTSEkDT9W">
-                    <FaDiscord className="mt-0.5 text-2xl" />
-                  </a>
+                <div className="xs:ml-6 mt-2 xs:mt-0 mx-auto flex">
+                  <div className="">
+                    <a href="mailto:staff@catalog.fyi" target="_blank" rel="noopener noreferrer">
+                      <BsEnvelopeFill className="mt-[5px] text-lg" />
+                    </a>
+                  </div>
+                  <div className="ml-4 sm:ml-6">
+                    <a href="https://discord.gg/BWTSEkDT9W" rel="noopener noreferrer">
+                      <FaDiscord className="mt-0.5 text-2xl" />
+                    </a>
+                  </div>
+                  <div className="ml-[7px] sm:ml-4 -mt-[3px] w-8 h-8">
+                    <a
+                      href="https://bsky.app/profile/catalog.fyi"
+                      className="w-full h-full"
+                      rel="noopener noreferrer"
+                    >
+                      <BlueskyLogo />
+                    </a>
+                  </div>
                 </div>
               </footer>
             </div>
