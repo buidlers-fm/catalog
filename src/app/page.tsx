d736adc2a298e7f934cfd6ee32780430c989b88a
rorysaur <ou.rory@gmail.com>
2024-03-13 14:52:03 -0400

    always redirect root to /home if signed in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { redirect } from "next/navigation"
+import { getCurrentUserProfile } from "lib/server/auth"
 import LandingPage from "app/components/homepage/LandingPage"
 
 export const dynamic = "force-dynamic"
 
 export default async function RootPage() {
+  const currentUserProfile = await getCurrentUserProfile()
+
+  if (currentUserProfile) redirect("/home")
+
   return <LandingPage />
 }
