buidlers-fm/catalog

commit baa36c063ca3c4e4a556db3d5025cc5f0bbaf6cb

save all the things (#155)
save any list, note, post, or comment. collects all saved items on a page at /saved, filterable by item type.

also:
+ splits InteractionObjectType.BookNote into .Note and .Post, so that the save (interaction) record can distinguish between these types (requires data migration on prod after deploy)
+ adds new path `/lists/[listId]` which redirects to `/users/[username]/lists/[listSlug]`, so that it's easier to link to a list with just its id. (needed for link to a saved comment's root object when it's a list)
+ fixes bug with `getCurrentUserProfile`'s `options` implementation where any passed `options` object would override the entire set of default options, rather than being handled individually. (ie, should be a merge instead of a replace)

---------

Co-authored-by: Brian McGue <mcgue.brian@gmail.com>

src/lib/api/likes.ts
@@ -10,7 +10,11 @@ enum UpdateLikeCountMode {
   Decrement,
 }
 
-const typesWithLikeCounts = [InteractionObjectType.BookNote, InteractionObjectType.Comment]
+const typesWithLikeCounts = [
+  InteractionObjectType.Note,
+  InteractionObjectType.Post,
+  InteractionObjectType.Comment,
+]
 
 async function getUpdateLikeCountParams(mode, params: any = {}) {
   const { likedObjectType, likedObjectId } = params
@@ -76,7 +80,10 @@ export async function findOrCreateLike({ likedObjectType, likedObjectId, userPro
 
     let updateLikeCountPromise
 
-    if (likedObjectType === InteractionObjectType.BookNote) {
+    if (
+      likedObjectType === InteractionObjectType.Note ||
+      likedObjectType === InteractionObjectType.Post
+    ) {
       updateLikeCountPromise = prisma.bookNote.update(updateLikeCountParams)
     } else if (likedObjectType === InteractionObjectType.Comment) {
       updateLikeCountPromise = prisma.comment.update(updateLikeCountParams)
@@ -110,7 +117,10 @@ export async function deleteLike(like) {
 
     let updateLikeCountPromise
 
-    if (like.objectType === InteractionObjectType.BookNote) {
+    if (
+      like.objectType === InteractionObjectType.Note ||
+      like.objectType === InteractionObjectType.Post
+    ) {
       updateLikeCountPromise = prisma.bookNote.update(updateLikeCountParams)
     } else if (like.objectType === InteractionObjectType.Comment) {
       updateLikeCountPromise = prisma.comment.update(updateLikeCountParams)
@@ -144,7 +154,10 @@ export async function deleteLikeByParams({ likedObjectType, likedObjectId, userP
 
     let updateLikeCountPromise
 
-    if (likedObjectType === InteractionObjectType.BookNote) {
+    if (
+      likedObjectType === InteractionObjectType.Note ||
+      likedObjectType === InteractionObjectType.Post
+    ) {
       updateLikeCountPromise = prisma.bookNote.update(updateLikeCountParams)
     } else if (likedObjectType === InteractionObjectType.Comment) {
       updateLikeCountPromise = prisma.comment.update(updateLikeCountParams)

src/lib/helpers/general.ts
@@ -76,6 +76,8 @@ export const getBookListsLink = (slug: string) => `/books/${slug}/lists`
 export const getListLink = (userProfile, slug: string) =>
   `/users/${userProfile.username}/lists/${slug}`
 
+export const getListLinkById = (listId: string) => `/lists/${listId}`
+
 export const getNewListLink = (userProfile) => `${getUserListsLink(userProfile.username)}/new`
 
 export const getEditListLink = (userProfile, slug: string) =>

src/lib/server/notifs.ts
@@ -1,4 +1,3 @@
-import humps from "humps"
 import prisma from "lib/prisma"
 import { reportToSentry } from "lib/sentry"
 import { commentParentTypeToNotificationObjectType } from "lib/helpers/general"
@@ -35,11 +34,8 @@ async function createNotifFromSource(source) {
 
   let notifiedUserProfileId
 
-  if (objectType === InteractionObjectType.BookNote || objectType === InteractionObjectType.List) {
-    const modelName = humps.camelize(objectType)
-
-    // @ts-ignore dynamic model name
-    const object = await prisma[modelName].findFirst({
+  if (objectType === InteractionObjectType.List) {
+    const object = await prisma.list.findFirst({
       where: {
         id: objectId,
       },
@@ -48,7 +44,25 @@ async function createNotifFromSource(source) {
     if (object) {
       notifiedUserProfileId = object.creatorId
     } else {
-      reportToSentry(new Error(`${objectType} not found, can't create notif`), {
+      reportToSentry(new Error(`List not found, can't create notif`), {
+        source,
+      })
+      return
+    }
+  } else if (
+    objectType === InteractionObjectType.Note ||
+    objectType === InteractionObjectType.Post
+  ) {
+    const object = await prisma.bookNote.findFirst({
+      where: {
+        id: objectId,
+      },
+    })
+
+    if (object) {
+      notifiedUserProfileId = object.creatorId
+    } else {
+      reportToSentry(new Error(`Note or post not found, can't create notif`), {
         source,
       })
       return

src/app/api/saves/[saveId]/route.ts
@@ -1,0 +1,32 @@
+import { NextResponse } from "next/server"
+import prisma from "lib/prisma"
+import { withApiHandling } from "lib/api/withApiHandling"
+import type { NextRequest } from "next/server"
+
+export const DELETE = withApiHandling(
+  async (_req: NextRequest, { params }) => {
+    const { routeParams, currentUserProfile: userProfile } = params
+    const { saveId } = routeParams
+
+    const save = await prisma.interaction.findFirst({
+      where: {
+        id: saveId,
+      },
+    })
+
+    if (save) {
+      if (save.agentId !== userProfile.id) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 403 })
+      }
+
+      await prisma.interaction.delete({
+        where: {
+          id: save.id,
+        },
+      })
+    }
+
+    return NextResponse.json({}, { status: 200 })
+  },
+  { requireJsonBody: false },
+)

src/app/api/saves/route.ts
@@ -1,0 +1,186 @@
+import { NextResponse } from "next/server"
+import humps from "humps"
+import prisma from "lib/prisma"
+import { withApiHandling } from "lib/api/withApiHandling"
+import { getCurrentUserProfile } from "lib/server/auth"
+import { getBookNotes } from "lib/server/bookNotes"
+import { decorateLists, decorateComments } from "lib/server/decorators"
+import BookNoteType from "enums/BookNoteType"
+import InteractionType from "enums/InteractionType"
+import InteractionAgentType from "enums/InteractionAgentType"
+import InteractionObjectType from "enums/InteractionObjectType"
+import type { NextRequest } from "next/server"
+
+export const GET = withApiHandling(
+  async () => {
+    const currentUserProfile = await getCurrentUserProfile({ requireSignedIn: true })
+
+    const saves = await prisma.interaction.findMany({
+      where: {
+        interactionType: InteractionType.Save,
+        agentId: currentUserProfile.id,
+        agentType: InteractionAgentType.User,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    })
+
+    const savedListIds = saves
+      .filter((save) => save.objectType === InteractionObjectType.List)
+      .map((save) => save.objectId)
+
+    async function getSavedLists() {
+      let savedLists = await prisma.list.findMany({
+        where: {
+          id: {
+            in: savedListIds,
+          },
+        },
+        include: {
+          listItemAssignments: {
+            orderBy: {
+              sortOrder: "asc",
+            },
+          },
+        },
+      })
+
+      savedLists = await decorateLists(savedLists, currentUserProfile)
+
+      return savedLists.reduce((acc, list) => {
+        acc[list.id] = list
+        return acc
+      }, {})
+    }
+
+    async function getSavedNotes() {
+      const savedNoteIds = saves
+        .filter((save) => save.objectType === InteractionObjectType.Note)
+        .map((save) => save.objectId)
+
+      const savedNotes = await getBookNotes({
+        noteTypes: [BookNoteType.JournalEntry],
+        moreFilters: {
+          id: {
+            in: savedNoteIds,
+          },
+        },
+        currentUserProfile,
+      })
+
+      return savedNotes.reduce((acc, note) => {
+        acc[note.id] = note
+        return acc
+      }, {})
+    }
+
+    async function getSavedPosts() {
+      const savedPostIds = saves
+        .filter((save) => save.objectType === InteractionObjectType.Post)
+        .map((save) => save.objectId)
+
+      const savedPosts = await getBookNotes({
+        noteTypes: [BookNoteType.Post],
+        moreFilters: {
+          id: {
+            in: savedPostIds,
+          },
+        },
+        currentUserProfile,
+      })
+
+      return savedPosts.reduce((acc, post) => {
+        acc[post.id] = post
+        return acc
+      }, {})
+    }
+
+    async function getSavedComments() {
+      const savedCommentIds = saves
+        .filter((save) => save.objectType === InteractionObjectType.Comment)
+        .map((save) => save.objectId)
+
+      let savedComments = await prisma.comment.findMany({
+        where: {
+          id: {
+            in: savedCommentIds,
+          },
+        },
+      })
+
+      savedComments = await decorateComments(savedComments, currentUserProfile)
+
+      return savedComments.reduce((acc, comment) => {
+        acc[comment.id] = comment
+        return acc
+      }, {})
+    }
+
+    const promises = [getSavedLists(), getSavedNotes(), getSavedPosts(), getSavedComments()]
+
+    const [savedListsById, savedNotesById, savedPostsById, savedCommentsById] = await Promise.all(
+      promises,
+    )
+
+    const savedObjects = saves
+      .map((save) => {
+        let savedObject
+
+        switch (save.objectType) {
+          case InteractionObjectType.List:
+            savedObject = savedListsById[save.objectId]
+            break
+          case InteractionObjectType.Note:
+            savedObject = savedNotesById[save.objectId]
+            break
+          case InteractionObjectType.Post:
+            savedObject = savedPostsById[save.objectId]
+            break
+          case InteractionObjectType.Comment:
+            savedObject = savedCommentsById[save.objectId]
+            break
+          default:
+            return null
+        }
+
+        if (!savedObject) return null
+
+        return {
+          ...savedObject,
+          save,
+        }
+      })
+      .filter(Boolean)
+
+    const resBody = humps.decamelizeKeys(savedObjects)
+
+    return NextResponse.json(resBody, { status: 200 })
+  },
+  { requireJsonBody: false },
+)
+
+export const POST = withApiHandling(async (_req: NextRequest, { params }) => {
+  const { reqJson, currentUserProfile: userProfile } = params
+  const { savedObjectId, savedObjectType } = reqJson
+  const createParams = {
+    agentId: userProfile.id,
+    agentType: InteractionAgentType.User,
+    interactionType: InteractionType.Save,
+    objectId: savedObjectId,
+    objectType: savedObjectType,
+  }
+
+  // upsert is used for findOrCreate in prisma
+  // passing an empty object in the update param will make no changes if the record exists
+  // https://www.prisma.io/docs/orm/prisma-client/queries/crud#update-or-create-records
+  const save = await prisma.interaction.upsert({
+    where: { agentId_agentType_interactionType_objectId_objectType: createParams },
+    create: createParams,
+    update: {},
+  })
+
+  const resBody = humps.decamelizeKeys(save)
+
+  return NextResponse.json(resBody, { status: 200 })
+})

src/app/books/[bookSlug]/lists/page.tsx
@@ -61,7 +61,12 @@ export default async function BookListsIndexPage({ params }) {
         {lists.length > 0 ? (
           <div className="">
             {lists.map((list) => (
-              <ListCard key={list.id} list={list} withByline />
+              <ListCard
+                key={list.id}
+                list={list}
+                currentUserProfile={currentUserProfile}
+                withByline
+              />
             ))}
           </div>
         ) : (

src/app/books/components/BookPage.tsx
@@ -620,7 +620,12 @@ export default function BookPage({
             <hr className="my-1 h-[1px] border-none bg-gray-300" />
             <div className="">
               {bookLists.map((list) => (
-                <ListCard key={list.id} list={list} withByline />
+                <ListCard
+                  key={list.id}
+                  list={list}
+                  currentUserProfile={currentUserProfile}
+                  withByline
+                />
               ))}
             </div>
           </div>

src/app/components/SavedItems.tsx
@@ -1,0 +1,161 @@
+"use client"
+
+import Link from "next/link"
+import { useState, useEffect } from "react"
+import { useUser } from "lib/contexts/UserContext"
+import api from "lib/api"
+import { getNoteLink, getPostLink, getListLinkById } from "lib/helpers/general"
+import EmptyState from "app/components/EmptyState"
+import LoadingSection from "app/components/LoadingSection"
+import BookNoteCard from "app/components/bookNotes/BookNoteCard"
+import BookLinkPostCard from "app/components/bookPosts/BookPostCard"
+import CommentCard from "app/components/comments/CommentCard"
+import ListCard from "app/components/lists/ListCard"
+import InteractionObjectType from "enums/InteractionObjectType"
+import CommentParentType from "enums/CommentParentType"
+
+enum Filter {
+  All = "all",
+  Note = InteractionObjectType.Note,
+  Post = InteractionObjectType.Post,
+  Comment = InteractionObjectType.Comment,
+  List = InteractionObjectType.List,
+}
+
+const filterToCopy = {
+  [Filter.All]: "all",
+  [Filter.Note]: "notes",
+  [Filter.Post]: "posts",
+  [Filter.Comment]: "comments",
+  [Filter.List]: "lists",
+}
+
+export default function SavedItems() {
+  const { currentUserProfile } = useUser()
+
+  const [savedItems, setSavedItems] = useState<any[]>()
+  const [currentFilter, setCurrentFilter] = useState<Filter>(Filter.All)
+  const [visibleItems, setVisibleItems] = useState<any[]>()
+
+  const fetchSavedItems = async () => {
+    const _savedItems = await api.saves.get()
+    setSavedItems(_savedItems)
+  }
+
+  useEffect(() => {
+    fetchSavedItems()
+  }, [])
+
+  useEffect(() => {
+    const _visibleItems = savedItems?.filter((item) => {
+      if (currentFilter === Filter.All) return true
+      return item.save.objectType === currentFilter
+    })
+
+    setVisibleItems(_visibleItems)
+  }, [savedItems, currentFilter])
+
+  if (!savedItems) {
+    return <LoadingSection />
+  }
+
+  if (savedItems.length === 0) {
+    return <EmptyState text="You haven't saved anything yet." />
+  }
+
+  return (
+    <>
+      <div className="flex justify-end items-baseline mt-4 text-gray-300 font-mulish">
+        filter by type:
+        <select
+          value={currentFilter}
+          onChange={(e) => setCurrentFilter(e.target.value as Filter)}
+          className="ml-2 bg-gray-900 text-white border-none rounded"
+        >
+          {Object.keys(filterToCopy).map((filter) => (
+            <option key={filter} value={filter}>
+              {filterToCopy[filter]}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {visibleItems && visibleItems.length > 0 ? (
+        visibleItems.map((savedItem) => (
+          <div key={savedItem.id} className="py-4 border-b border-b-gray-300 last:border-none">
+            {savedItem.save.objectType === InteractionObjectType.Note && (
+              <div className="mt-2">
+                <div className="cat-eyebrow">note</div>
+                <BookNoteCard
+                  note={savedItem}
+                  currentUserProfile={currentUserProfile || undefined}
+                  onEditSuccess={fetchSavedItems}
+                  onDeleteSuccess={fetchSavedItems}
+                  onSaveUnsave={fetchSavedItems}
+                />
+              </div>
+            )}
+
+            {savedItem.save.objectType === InteractionObjectType.Post && (
+              <div className="mt-2">
+                <div className="cat-eyebrow">post</div>
+                <BookLinkPostCard
+                  post={savedItem}
+                  currentUserProfile={currentUserProfile || undefined}
+                  onEditSuccess={fetchSavedItems}
+                  onDeleteSuccess={fetchSavedItems}
+                  onSaveUnsave={fetchSavedItems}
+                />
+              </div>
+            )}
+
+            {savedItem.save.objectType === InteractionObjectType.Comment && (
+              <div className="mt-2">
+                <div className="cat-eyebrow">
+                  comment on{" "}
+                  {savedItem.rootObjectType === CommentParentType.Note && (
+                    <Link href={getNoteLink(savedItem.rootObjectId)} className="cat-link">
+                      a note
+                    </Link>
+                  )}
+                  {savedItem.rootObjectType === CommentParentType.Post && (
+                    <Link href={getPostLink(savedItem.rootObjectId)} className="cat-link">
+                      a post
+                    </Link>
+                  )}
+                  {savedItem.rootObjectType === CommentParentType.List && (
+                    <Link href={getListLinkById(savedItem.rootObjectId)} className="cat-link">
+                      a list
+                    </Link>
+                  )}
+                  {/* the below case shouldn't exist (where root object is a comment), but in case of a bug */}
+                  {savedItem.rootObjectType === CommentParentType.Comment && "a comment"}
+                </div>
+                <CommentCard
+                  comment={savedItem}
+                  currentUserProfile={currentUserProfile || undefined}
+                  onDelete={fetchSavedItems}
+                  onSaveUnsave={fetchSavedItems}
+                />
+              </div>
+            )}
+
+            {savedItem.save.objectType === InteractionObjectType.List && (
+              <div className="mt-2">
+                <div className="cat-eyebrow -mb-4">list</div>
+                <ListCard
+                  list={savedItem}
+                  currentUserProfile={currentUserProfile || undefined}
+                  withByline
+                  onSaveUnsave={fetchSavedItems}
+                />
+              </div>
+            )}
+          </div>
+        ))
+      ) : (
+        <EmptyState text="No items match the current filter." />
+      )}
+    </>
+  )
+}

commit 814033536670ba033bd3c5a138e759d993c979b2

fix: notifs for likes on notes/posts (#162)
this is another notifs bug caused by the type mismatch introduced in #155 .

src/lib/api/likes.ts
@@ -3,7 +3,6 @@ import { createNotifFromLike } from "lib/server/notifs"
 import InteractionType from "enums/InteractionType"
 import InteractionAgentType from "enums/InteractionAgentType"
 import InteractionObjectType from "enums/InteractionObjectType"
-import NotificationObjectType from "enums/NotificationObjectType"
 
 enum UpdateLikeCountMode {
   Increment,
@@ -94,7 +93,7 @@ export async function findOrCreateLike({ likedObjectType, likedObjectId, userPro
     createdLike = await createLikePromise
   }
 
-  if (Object.values(NotificationObjectType).includes(likedObjectType)) {
+  if (likedObjectType !== InteractionObjectType.Book) {
     await createNotifFromLike(createdLike)
   }
 

src/lib/helpers/general.ts
@@ -7,6 +7,7 @@ import { reportToSentry } from "lib/sentry"
 import { BASE_URLS_BY_ENV } from "lib/constants/urls"
 import CommentParentType from "enums/CommentParentType"
 import NotificationObjectType from "enums/NotificationObjectType"
+import InteractionObjectType from "enums/InteractionObjectType"
 
 export const fetchJson = async (url: string | URL, options: any = {}) => {
   const TIMEOUT = 10_000 // 10 seconds
@@ -246,4 +247,13 @@ export function commentParentTypeToNotificationObjectType(parentType: string) {
   }
 
   return specialMappings[parentType] || parentType
+}
+
+export function interactionObjectTypeToNotificationObjectType(objectType: string) {
+  const specialMappings = {
+    [InteractionObjectType.Note]: NotificationObjectType.BookNote,
+    [InteractionObjectType.Post]: NotificationObjectType.BookNote,
+  }
+
+  return specialMappings[objectType] || objectType
 }

src/lib/server/notifs.ts
@@ -1,6 +1,9 @@
 import prisma from "lib/prisma"
 import { reportToSentry } from "lib/sentry"
-import { commentParentTypeToNotificationObjectType } from "lib/helpers/general"
+import {
+  commentParentTypeToNotificationObjectType,
+  interactionObjectTypeToNotificationObjectType,
+} from "lib/helpers/general"
 import NotificationType from "enums/NotificationType"
 import NotificationAgentType from "enums/NotificationAgentType"
 import NotificationObjectType from "enums/NotificationObjectType"
@@ -131,7 +134,9 @@ async function createNotifFromSource(source) {
 }
 
 async function createNotifFromLike(likeInteraction) {
-  const { id, agentId, objectId, objectType } = likeInteraction
+  const { id, agentId, objectId, objectType: interactionObjectType } = likeInteraction
+
+  const objectType = interactionObjectTypeToNotificationObjectType(interactionObjectType)
 
   return createNotifFromSource({
     id,