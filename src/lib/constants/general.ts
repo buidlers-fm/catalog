f32de91d6e668df4f4b0a241394a9ecbade50a3d
Rory Ou <ou.rory@gmail.com>
2024-05-30 03:44:30 -0400

    call wikidata api for author name, bio, wikipedia url (#190)
    + adds wikidata/wikipedia client object for looking up any item in general
    + our OL client calls the wikidata client (if wikidata id is present) for author name, bio, and wikipedia url
    + `OpenLibrary.getFullBook` also gets the author's wikidata english name if possible. this means that visiting a book's page will fix/update the author name (change it from OL to wikidata name) if possible.
      + the upshot is that the author's name can be from wikidata if the book is interacted with from the book page or "create list from book", but not in search results and not if the book is created via a bulk list create, or from the global create modal (although this latter might be doable as a followup).
    + adds helper to attach a user agent header with catalog's contact info to our outgoing requests- useful for calling api integrations.

---
+++ src/lib/constants/general.ts
@@ -0,0 +1,4 @@
+export const USER_AGENT_HEADERS = {
+  "User-Agent": "catalog.fyi/v0 (staff@catalog.fyi) Next.js/14",
+  "Api-User-Agent": "catalog.fyi/v0 (staff@catalog.fyi) Next.js/14",
+}
---
+++ src/lib/wikidata.ts
@@ -0,0 +1,44 @@
+import { fetchJsonWithUserAgentHeaders } from "lib/helpers/general"
+
+const WIKIDATA_BASE_URL = "https://wikidata.org/w/rest.php/wikibase/v0"
+const WIKIDATA_ITEM_BASE_URL = `${WIKIDATA_BASE_URL}/entities/items`
+
+const WIKIPEDIA_BASE_URL = "https://en.wikipedia.org/api/rest_v1"
+const WIKIPEDIA_SUMMARY_BASE_URL = `${WIKIPEDIA_BASE_URL}/page/summary`
+
+const GET_ITEM_DEFAULT_OPTIONS = {
+  compact: false,
+}
+
+const Wikidata = {
+  getItem: async (id: string, options: any = GET_ITEM_DEFAULT_OPTIONS) => {
+    const { compact } = options
+
+    const url = `${WIKIDATA_ITEM_BASE_URL}/${id}`
+    const item = await fetchJsonWithUserAgentHeaders(url)
+
+    const { labels, sitelinks } = item
+
+    const name = labels.en
+    const { title: siteTitle, url: siteUrl } = sitelinks.enwiki
+
+    if (compact) {
+      return { name, siteTitle, siteUrl }
+    }
+
+    const siteUrlFragment = sitelinks.enwiki.url.split("/").pop()
+    const wikipediaSummaryUrl = `${WIKIPEDIA_SUMMARY_BASE_URL}/${siteUrlFragment}`
+
+    const wikipediaSummaryRes = await fetchJsonWithUserAgentHeaders(wikipediaSummaryUrl)
+    const wikipediaSummary = wikipediaSummaryRes.extract
+
+    return {
+      name,
+      siteTitle,
+      siteUrl,
+      summary: wikipediaSummary,
+    }
+  },
+}
+
+export default Wikidata
