178cc6f9b74c08e947f03fa36bbefd3b2dad902a
rorysaur <ou.rory@gmail.com>
2024-07-15 22:37:37 -0400

    person page: grid view for books

--- src/app/lists/components/ListBook.tsx
+++ src/app/lists/components/ListBook.tsx
@@ -6,7 +6,7 @@ import { getSelectorsByUserAgent } from "react-device-detect"
 import { Tooltip } from "react-tooltip"
 import { FaNoteSticky } from "react-icons/fa6"
 import { GiOpenBook } from "react-icons/gi"
-import { getBookLink } from "lib/helpers/general"
+import { getBookLinkAgnostic } from "lib/helpers/general"
 import { truncateString } from "lib/helpers/strings"
 import BookCoverOverlay from "app/components/books/BookCoverOverlay"
 import BookTooltip from "app/components/books/BookTooltip"
@@ -25,6 +25,7 @@ export default function ListBook({
   rank = 0,
   widthClasses = "",
   heightClasses = "",
+  detail = undefined,
 }) {
   const [imgLoaded, setImgLoaded] = useState<boolean>(false)
   const [isMobile, setIsMobile] = useState(false)
@@ -46,13 +47,15 @@ export default function ListBook({
   const widthClassesToUse = widthClasses || (isFavorite ? favoriteBookWidths : defaultWidths)
   const heightClassesToUse = heightClasses || (isFavorite ? favoriteBookHeights : defaultHeights)
 
+  const id = book.id || book.openLibraryWorkId
+
   return (
-    <div key={book.id} className="flex flex-col items-center justify-center">
+    <div key={id} className="flex flex-col items-center justify-center">
       <div className={`grow flex items-center ${widthClassesToUse} h-auto my-8 mx-auto sm:my-4`}>
         <div className="relative group">
           <LinkOrDiv
             // @ts-ignore this is a weird case, just let it be
-            href={isMobile ? undefined : getBookLink(book.slug)}
+            href={isMobile ? undefined : getBookLinkAgnostic(book)}
           >
             {book.coverImageUrl && !imgLoaded && (
               <CoverPlaceholder
@@ -66,7 +69,7 @@ export default function ListBook({
               <img
                 ref={imgRef}
                 src={book.coverImageUrl}
-                id={`book-${book.id}`}
+                id={`book-${id}`}
                 className={`w-full ${imgLoaded ? "block" : "hidden"} rounded-sm`}
                 alt={`${book.title} cover`}
                 onLoad={() => setImgLoaded(true)}
@@ -86,7 +89,9 @@ export default function ListBook({
         </div>
       </div>
 
-      <BookTooltip book={book} anchorSelect={`#book-${book.id}`} />
+      <BookTooltip book={book} anchorSelect={`#book-${id}`} />
+
+      {detail && <div className="-mt-2 text-gray-300 text-sm font-mulish">{detail}</div>}
 
       {isRanked && (
         <span className="flex justify-center w-1/2 border-b border-gray-700">{rank}</span>
@@ -95,25 +100,31 @@ export default function ListBook({
   )
 }
 
-const CoverPlaceholder = ({ book, loading = false, widthClasses, heightClasses }) => (
-  <div
-    id={`book-${book.id}`}
-    className={`${widthClasses} ${heightClasses} p-2 flex flex-col items-center justify-center border-2 border-gray-500 box-border rounded font-mulish text-center text-sm text-gray-200`}
-  >
-    {loading ? (
-      "Loading..."
-    ) : (
-      <>
-        <GiOpenBook className="hidden sm:block mb-4 sm:mb-2 text-8xl sm:text-4xl text-gray-500" />
-        <div className="mb-2 sm:mb-0">{truncateString(book.title, 20)}</div>
-        <div>{truncateString(book.authorName, 20)}</div>
-      </>
-    )}
-  </div>
-)
+const CoverPlaceholder = ({ book, loading = false, widthClasses, heightClasses }) => {
+  const id = book.id || book.openLibraryWorkId
+
+  return (
+    <div
+      id={`book-${id}`}
+      className={`${widthClasses} ${heightClasses} p-2 flex flex-col items-center justify-center border-2 border-gray-500 box-border rounded font-mulish text-center text-sm text-gray-200`}
+    >
+      {loading ? (
+        "Loading..."
+      ) : (
+        <>
+          <GiOpenBook className="hidden sm:block mb-4 sm:mb-2 text-8xl sm:text-4xl text-gray-500" />
+          <div className="mb-2 sm:mb-0">{truncateString(book.title, 20)}</div>
+          <div>{truncateString(book.authorName, 20)}</div>
+        </>
+      )}
+    </div>
+  )
+}
 
 const ListNote = ({ book, note, positionClass }) => {
-  const tooltipAnchorId = `book-${book.id}-featured-note`
+  const id = book.id || book.openLibraryWorkId
+
+  const tooltipAnchorId = `book-${id}-featured-note`
 
   return (
     <div className={`absolute ${positionClass}`}>
@@ -130,7 +141,7 @@ const ListNote = ({ book, note, positionClass }) => {
           <div className="font-bold">{truncateString(`${book.title}`, 40)}</div>
           <div className="mb-2">{truncateString(`by ${book.authorName}`, 40)}</div>
           <CustomMarkdown markdown={note} />
-          <Link href={getBookLink(book.slug)} className="block mt-2 font-mulish underline">
+          <Link href={getBookLinkAgnostic(book)} className="block mt-2 font-mulish underline">
             go to book page
           </Link>
         </div>
