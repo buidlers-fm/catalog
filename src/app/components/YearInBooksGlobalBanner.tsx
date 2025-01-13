9b9f4975ef7f294a016b7ccfaa8f240b0bd877a2
Robyn Choi <robyn3choi@gmail.com>
2025-01-12 20:45:05 -0800

    year in books banner local storage

--- src/app/components/YearInBooksGlobalBanner.tsx
+++ src/app/components/YearInBooksGlobalBanner.tsx
@@ -1,15 +1,40 @@
 "use client"
 
 import Link from "next/link"
+import { useRouter, usePathname, useSearchParams } from "next/navigation"
 import { useUser } from "lib/contexts/UserContext"
 import { BsXLg } from "react-icons/bs"
-import { LuPartyPopper } from "react-icons/lu"
 import { GiPartyPopper } from "react-icons/gi"
+import { getLocalStorage, setLocalStorage } from "lib/localstorage"
+import { useEffect, useState } from "react"
+
+const YEAR_IN_BOOKS_2024_LOCALSTORAGE_KEY = "catalog__year-in-books-2024-seen"
 
 export default function YearInBooksGlobalBanner() {
   const { currentUser } = useUser()
+  const pathname = usePathname()
+  const [showBanner, setShowBanner] = useState(false)
 
-  if (!currentUser) return null
+  useEffect(() => {
+    const isBannerSeen = getLocalStorage(YEAR_IN_BOOKS_2024_LOCALSTORAGE_KEY)
+    if (!isBannerSeen) {
+      setShowBanner(true)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (currentUser && pathname === `/users/${currentUser.username}/year/2024`) {
+      setShowBanner(false)
+      setLocalStorage(YEAR_IN_BOOKS_2024_LOCALSTORAGE_KEY, true)
+    }
+  }, [pathname, currentUser])
+
+  if (!currentUser || !showBanner) return null
+
+  const onCloseBanner = () => {
+    setShowBanner(false)
+    setLocalStorage(YEAR_IN_BOOKS_2024_LOCALSTORAGE_KEY, true)
+  }
 
   return (
     <div className="flex mx-4 xs:mx-8 border border-gold-500 p-5 font-newsreader text-lg rounded mb-4">
@@ -17,12 +42,12 @@ export default function YearInBooksGlobalBanner() {
       <div className="mt-[1px]">
         <strong className="text-xl">Your catalog year in review is here! </strong> <br />
         See{" "}
-        <Link className="cat-underline" href={`/users/${currentUser?.username}/2024`}>
+        <Link className="cat-underline" href={`/users/${currentUser.username}/year/2024`}>
           your 2024 in books
         </Link>{" "}
         for all the stats on your reading and your activity on catalog over the past year.
       </div>
-      <button className="ml-auto pl-4 h-fit">
+      <button className="ml-auto pl-4 h-fit" onClick={onCloseBanner}>
         <BsXLg />
       </button>
     </div>
